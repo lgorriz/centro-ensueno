cbd9b29bb42090e179f76daaf50912d55d30f3a4
Leandro <gorrizleandro@gmail.com>
2023-04-28 20:14:25 -0300

    Updates

---
+++ precache-manifest.9a87ba9e9fa73efc1b1242b5b3275292.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45f6a882c5e5d68760a0387b648d2f4b",
+    "url": "/centro-ensueno/index.html"
+  },
+  {
+    "revision": "14301aa4e6668c35610b",
+    "url": "/centro-ensueno/static/css/main.ab83a158.chunk.css"
+  },
+  {
+    "revision": "e7ad440b5c3e7dc9d4e0",
+    "url": "/centro-ensueno/static/js/2.7a0a1157.chunk.js"
+  },
+  {
+    "revision": "1d8c80ef742f8ffe0bb17fb206660c60",
+    "url": "/centro-ensueno/static/js/2.7a0a1157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14301aa4e6668c35610b",
+    "url": "/centro-ensueno/static/js/main.3b4f4cdd.chunk.js"
+  },
+  {
+    "revision": "7a6956e196ccd2e4ccc8856708b02847",
+    "url": "/centro-ensueno/static/js/main.3b4f4cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cfb3c2699192cc3216",
+    "url": "/centro-ensueno/static/js/runtime-main.f3722613.js"
+  },
+  {
+    "revision": "5919e5c842f7d222ddfd03f9810d56b3",
+    "url": "/centro-ensueno/static/media/actividades.5919e5c8.jpg"
+  },
+  {
+    "revision": "8c5a92106c3ab172f85ef5bd9caa1538",
+    "url": "/centro-ensueno/static/media/actividades2.8c5a9210.jpg"
+  },
+  {
+    "revision": "cd3b6170ae1d4b2defe9a4f574c35490",
+    "url": "/centro-ensueno/static/media/argon-react.cd3b6170.png"
+  },
+  {
+    "revision": "78137a5d9e115ce4b7c48c584eddefb8",
+    "url": "/centro-ensueno/static/media/banio.78137a5d.jpg"
+  },
+  {
+    "revision": "4fdb5044b3a4127d231de54c7206ec40",
+    "url": "/centro-ensueno/static/media/banio2.4fdb5044.jpg"
+  },
+  {
+    "revision": "31e5c547fd0e3fcb5b6d6c21c274d821",
+    "url": "/centro-ensueno/static/media/banio3.31e5c547.jpg"
+  },
+  {
+    "revision": "48b9c4009b2cefaddee1f6d1bfd71a3a",
+    "url": "/centro-ensueno/static/media/close-up-of-executives-sitting-at-the-table.48b9c400.jpg"
+  },
+  {
+    "revision": "600f48a1b93116987df8137c77c8d8a9",
+    "url": "/centro-ensueno/static/media/cocina.600f48a1.jpg"
+  },
+  {
+    "revision": "36f5bd81d323b826b70cdd409dcad236",
+    "url": "/centro-ensueno/static/media/cocina2.36f5bd81.jpg"
+  },
+  {
+    "revision": "a6c623ee1577fc88cf64029ef9da8084",
+    "url": "/centro-ensueno/static/media/comedor.a6c623ee.jpg"
+  },
+  {
+    "revision": "dc444ebee7504f058f1025ab9d230caa",
+    "url": "/centro-ensueno/static/media/computacion.dc444ebe.jpg"
+  },
+  {
+    "revision": "2d4907d58f568f8dde372475df1fa8ff",
+    "url": "/centro-ensueno/static/media/entrada.2d4907d5.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/centro-ensueno/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/centro-ensueno/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/centro-ensueno/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/centro-ensueno/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/centro-ensueno/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "907a9030dcfce1d17f988ceb17ddc1b4",
+    "url": "/centro-ensueno/static/media/frente1.907a9030.jpg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/centro-ensueno/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "2ebc9ef47e9399db30e0e552a91e77ac",
+    "url": "/centro-ensueno/static/media/logo1.2ebc9ef4.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/centro-ensueno/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/centro-ensueno/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/centro-ensueno/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/centro-ensueno/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/centro-ensueno/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "4bb7f4a1740d0b24871bcfc6b54eac5e",
+    "url": "/centro-ensueno/static/media/oficina1.4bb7f4a1.jpg"
+  },
+  {
+    "revision": "8838ff0e0eb8ffdb8d479b3ca43501f2",
+    "url": "/centro-ensueno/static/media/oficina2.8838ff0e.jpg"
+  },
+  {
+    "revision": "8d0b0067909a3a5c108dac01ff3524b5",
+    "url": "/centro-ensueno/static/media/oficina3.8d0b0067.jpg"
+  },
+  {
+    "revision": "821690a89d5eb32f1bd890952ff03017",
+    "url": "/centro-ensueno/static/media/servicios.821690a8.jpg"
+  },
+  {
+    "revision": "545a74167c0fe654e13d5754830810de",
+    "url": "/centro-ensueno/static/media/values.545a7416.png"
+  }
+]);
